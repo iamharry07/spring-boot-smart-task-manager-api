3c3c7ff20cafcbf002f9b0c75402f0f40e815923
Mohd Saqlain Ansari <140109652+iamharry07@users.noreply.github.com>
2026-08-04 16:01:43 +0530

    Add pom.xml and update gitignore

---
+++ src/main/java/com/saqlain/SmartTaskManagerAPI/SmartTaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.saqlain.SmartTaskManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saqlain/SmartTaskManagerAPI/SmartTaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saqlain.SmartTaskManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
