6947ad97a17006c139d24e91fb7c5f57dfad3a93
Mohd Saqlain Ansari <140109652+iamharry07@users.noreply.github.com>
2026-08-08 21:11:31 +0530

    Role Repo Implemented

---
+++ src/main/java/com/saqlain/SmartTaskManagerAPI/repository/RoleRepo.java
@@ -0,0 +1,13 @@
+package com.saqlain.SmartTaskManagerAPI.repository;
+
+import com.saqlain.SmartTaskManagerAPI.entity.Role;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleRepo {
+
+
+    private Role role;
+
+
+}
